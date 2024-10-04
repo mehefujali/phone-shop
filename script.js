08a202b521530c109803b2d26b320ddfb677e655
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-04 20:14:13 +0530

    search added

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-      .then(res => res.json())
-      .then(data => displayAllMobileData(data.data))
-      .catch(err => console.error(err))
+const lodeData = (id = 12) => {
+      fetch(`https://openapi.programming-hero.com/api/phones?search=${id}`)
+            .then(res => res.json())
+            .then(data => displayAllMobileData(data.data))
+            .catch(err => console.error(err))
+}
+
+lodeData()
 
 
+const searchInput = document.getElementById('searchInput')
+searchInput.addEventListener('keyup', (event) => {
+      lodeData(searchInput.value)
+})
 
 // {
 //       "brand": "Apple ",
@@ -17,19 +25,21 @@ fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
 const displayAllMobileData = (data) => {
 
       const mobileCards = document.getElementById('cards')
+      mobileCards.innerHTML = ``
       data.map(phone => {
             const { brand, phone_name, slug, image } = phone
             const phoneCard = document.createElement('div')
-            phoneCard.classList = "w-full flex flex-col justify-between items-center gap-5 "
+            phoneCard.classList = "w-full h-full border flex flex-col justify-between  gap-5 text-start p-6 rounded-lg  shadow-md"
             phoneCard.innerHTML = `
-                   <div> <image  src="${image}"></div>
+                   <div class=" flex justify-center"> <image  src="${image}"></div>
                   <div>
                   <h1 class=" font-bold text-2xl">${phone_name}</h1>
-                  <p class="">${brand}</p>
-
+                  <p class=" text-sm border px-4  rounded-full w-fit mt-2">${brand}</p>
+                 <div class=" flex w-full justify-end mt-6"> <button class=" btn ">Vew detels</button></div>
                   </div>
             `
             mobileCards.appendChild(phoneCard)
       })
 
 }
+
